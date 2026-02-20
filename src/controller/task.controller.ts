917e091f12cd4f15c7ddf9c1ff778ee32b28745b
Joshua Alfonso <Joshua025icloud@gmail.com>
2026-02-20 10:45:03 +0800

    task assignees

--- src/controller/task.controller.ts
+++ src/controller/task.controller.ts
@@ -17,6 +17,39 @@ export const getTaskByProjectController = async (c: any) => {
             }, 400);
         }
 
+            // const [rows]: any = await pool.query(`
+            //     SELECT 
+            //         t.task_id,
+            //         t.title,
+            //         t.description,
+            //         t.project_id,
+            //         p.project_name,
+            //         t.task_status_id,
+            //         ts.task_status_name,
+            //         t.task_priority_id,
+            //         tp.task_priority_name,
+            //         t.start_date,
+            //         t.end_date,
+            //         t.completed_at,
+            //         t.created_at
+            //     FROM 
+            //         task t
+            //     LEFT JOIN
+            //         project p
+            //     ON
+            //         t.project_id = p.project_id
+            //     LEFT JOIN
+            //         task_status ts
+            //     ON
+            //         t.task_status_id = ts.task_status_id
+            //     LEFT JOIN
+            //         task_priority tp
+            //     ON
+            //         t.task_priority_id = tp.task_priority_id
+            //     WHERE 
+            //         t.project_id = ?
+            // `, [project_id])
+
             const [rows]: any = await pool.query(`
                 SELECT 
                     t.task_id,
@@ -31,24 +64,34 @@ export const getTaskByProjectController = async (c: any) => {
                     t.start_date,
                     t.end_date,
                     t.completed_at,
-                    t.created_at
-                FROM 
-                    task t
-                LEFT JOIN
-                    project p
-                ON
-                    t.project_id = p.project_id
-                LEFT JOIN
-                    task_status ts
-                ON
-                    t.task_status_id = ts.task_status_id
-                LEFT JOIN
-                    task_priority tp
-                ON
-                    t.task_priority_id = tp.task_priority_id
-                WHERE 
-                    t.project_id = ?
-            `, [project_id])
+                    t.created_at,
+
+                    COALESCE(
+                        JSON_ARRAYAGG(
+                            IF(u.user_id IS NOT NULL,
+                                JSON_OBJECT(
+                                    'user_id', u.user_id,
+                                    'user_fullname', u.name
+                                ),
+                                NULL
+                            )
+                        ),
+                        JSON_ARRAY()
+                    ) AS assignees
+
+                FROM task t
+
+                LEFT JOIN project p ON t.project_id = p.project_id
+                LEFT JOIN task_status ts ON t.task_status_id = ts.task_status_id
+                LEFT JOIN task_priority tp ON t.task_priority_id = tp.task_priority_id
+                LEFT JOIN task_assignees ta ON t.task_id = ta.task_id
+                LEFT JOIN user u ON ta.user_id = u.user_id
+
+                WHERE t.project_id = ?
+
+                GROUP BY t.task_id
+            `, [project_id]);
+
 
         return c.json(rows, 200);
 
@@ -67,27 +110,50 @@ export const getTaskByProjectController = async (c: any) => {
 
 export const CreateTaskController = async (c: any) => {
 
+    const connection = await pool.getConnection();
+
     try {
+
+        await connection.beginTransaction();
+
         const { 
             project_id, 
             title, 
             description, 
             task_priority_id, 
             start_date, 
-            end_date 
+            end_date,
+            assignees    
         } = c.req.valid('json');
 
         const user = c.get("user");
         const created_by = user.user_id;
 
 
-        await pool.query(`
+        const [result]:any = await pool.query(`
             INSERT 
                 INTO task (project_id, title, description, task_priority_id, start_date, end_date, created_by)
             VALUES 
                 (?, ?, ?, ?, ?, ?, ?)
         `, [project_id, title, description, task_priority_id, start_date, end_date, created_by]);
 
+        const task_id = result.insertId;
+
+        if (assignees && assignees.length > 0) {
+            const values = assignees.map((user_id: number | string) => [
+                task_id,
+                Number(user_id)
+            ]);
+
+            await pool.query(`
+                INSERT 
+                    INTO task_assignees (task_id, user_id)
+                VALUES 
+                    ?
+            `, [values]);
+        }
+
+        await connection.commit();
 
         return c.json({
             success: true,
@@ -98,10 +164,17 @@ export const CreateTaskController = async (c: any) => {
 
     catch (error) {
         console.error(error);
+
+        await connection.rollback();
+
         return c.json({ 
             success: false,
             message: 'Failed to create task' 
         }, 500);
+    }
+
+    finally {
+        connection.release(); // always release
     }
 
 }
--- src/schema/task.schema.ts
+++ src/schema/task.schema.ts
@@ -11,10 +11,10 @@ export const createTaskSchema = z.object({
   project_id: z.number(),
   title: z.string(),
   description: z.string().min(10, 'Description is too short.'),
-//   task_status_id: z.number(),
   task_priority_id: z.number(),
   start_date: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, 'Invalid date format'),
   end_date: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, 'Invalid date format'),
+  assignees: z.array(z.string())
 })
 
 export type CreateTask = z.infer<typeof createTaskSchema>
